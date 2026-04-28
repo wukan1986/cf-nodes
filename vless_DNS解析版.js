9db830518da6b4932d96a2ceed70e9543f60f1e3
Kan <wu-kan@163.com>
2026-04-28 19:03:11 +0800

    防DDoS强刷DNS

--- vless_DNS解析版.js
+++ vless_DNS解析版.js
@@ -1,59 +1,75 @@
 ﻿const 必定要修改VL密钥 = '12345678-1234-1234-1234-123456789012';
 
 export default {
-	async fetch(访问请求) {
-		const url = new URL(访问请求.url);
-		if (访问请求.headers.get('Upgrade') === 'websocket') {
-			const ips = url.searchParams.getAll('ip');
-			if (ips) {
-				const 当前时间 = new Date();
-				const 需要刷新缓存 = (反代MAP.size === 0) || (url.search !== 链接SEARCH) || (当前时间.getTime() - 缓存时间.getTime() > 1000 * 60 * 10);//10分钟
-				if (需要刷新缓存 && !正在刷新) {
-					正在刷新 = true; 链接SEARCH = url.search; 缓存时间 = 当前时间;
-					for (const ip of ips) {
-						const u = new URL('url://' + ip.replace('colo', 访问请求.cf.colo).toLowerCase());
-						try {
-							if (isValidDomain(u.hostname)) {
-								const dnsRecords = (await Promise.all([
-									getDnsRecord(u.hostname, 'A'),
-									getDnsRecord(u.hostname, 'AAAA').then(rr => rr.map(r => `[${r}]`)),
-								])).flat().sort(() => Math.random() - 0.5);
-								dnsRecords.forEach(ip => 反代MAP.has(ip) || 反代MAP.set(ip, { 端口: +u.port || 443, 失败次数: 0 }));
-								console.log(u.hostname, '缓存刷新完成', '新IP数量:', dnsRecords.length, '共缓存IP数量:', 反代MAP.size);
-								//  [ERROR] Uncaught Error: internal error; reference = e99q54q6ivev4r4t8trolj61 为何？
-							} else { 反代MAP.set(u.hostname, { 端口: +u.port || 443, 失败次数: -1 }); }
-						} catch (error) { console.error(u.hostname, '刷新缓存失败', error); }
-					}
-					正在刷新 = false;
-				}
-			}
-			return await 升级WS请求();
-		}
-		return new Response(`Not Found. ${访问请求.cf.country}, ${访问请求.cf.region}, ${访问请求.cf.colo}`, { status: 404 });
+	async fetch(request) {
+		if (request.headers.get('Upgrade') === 'websocket') { return await 升级WS请求(request); }
+		return new Response(`Not Found. ${request.cf.country}, ${request.cf.region}, ${request.cf.colo}`, { status: 404 });
 	},
 };
-async function 升级WS请求() {
+async function 查询DNS(request) {
+	if (正在刷新) return;
+	const url = new URL(request.url);
+	const ips = url.searchParams.getAll('ip');
+	if (ips.length === 0) return;
+	const 当前时间 = new Date();
+	let 分钟差 = (当前时间.getTime() - 缓存时间.getTime()) / 60000;
+	let reset = url.searchParams.get('reset'); // 如果不设重置码，长时间后，只有池中数据不多时才刷新
+	if (reset) {
+		reset = +reset.slice(0, 1) || 0;
+		if (分钟差 > 180) {
+			重置码.clear(); 重置码.add(reset); 反代MAP.clear();  // 3小时后才能刷新
+		} else if (!重置码.has(reset)) {
+			重置码.add(reset); 反代MAP.clear(); 分钟差 = 180; // 新重置码才刷新，刷完后分钟差小了
+		}
+	}
+	if (反代MAP.size >= 30 || 分钟差 <= 10) return; // 缓存太多，或时间太近都不查询
+	正在刷新 = true; console.log('正在刷新缓存...');
+	try {
+		for (const ip of ips) {
+			const u = new URL('url://' + ip.replace('colo', request.cf.colo).toLowerCase());
+			try {
+				if (isIP(u.hostname)) {
+					反代MAP.delete(u.hostname); 反代MAP.set(u.hostname, { 端口: +u.port || 443, 失败次数: -1 }); // IP移动到最后
+				} else {
+					const dnsRecords = (await Promise.all([
+						getDnsRecord(u.hostname, 'A'),
+						getDnsRecord(u.hostname, 'AAAA').then(rr => rr.map(r => `[${r}]`)),
+					])).flat().sort(() => Math.random() - 0.5);
+					dnsRecords.forEach(ip => 反代MAP.has(ip) || 反代MAP.set(ip, { 端口: +u.port || 443, 失败次数: 0 }));
+					console.log(u.hostname, '缓存刷新完成', '新IP数量:', dnsRecords.length, '共缓存IP数量:', 反代MAP.size);
+				}
+			} catch (error) { console.error(u.hostname, '刷新缓存失败', error); }
+		}
+		缓存时间 = new Date();
+	}
+	catch (error) { console.error('DNS解析失败', error); }
+	finally { 正在刷新 = false; }
+}
+async function 升级WS请求(request) {
 	const [客户端, WS接口] = Object.values(new WebSocketPair());
 	WS接口.accept();
 	WS接口.binaryType = 'arraybuffer';
 	WS接口.send(new Uint8Array([0, 0]));
-	启动传输管道(WS接口);
+	启动传输管道(WS接口, request);
 	return new Response(null, { status: 101, webSocket: 客户端 });
 }
-async function 启动传输管道(WS接口) {
+async function 启动传输管道(WS接口, request) {
 	let TCP接口, 传输数据, 首包数据 = true, 处理队列 = Promise.resolve();
 	WS接口.addEventListener('message', (event) => {
 		处理队列 = 处理队列.then(async () => {
 			try {
 				if (首包数据) {
 					首包数据 = false;
-					await 解析VL标头(event.data);
+					await 解析VL标头(event.data, request);
 				} else { await 传输数据.write(event.data); }
 			} catch (error) { console.log(error.message); WS接口?.close(1000, error.message) }
 		});
 	});
-	async function 解析VL标头(VL数据) {
+	async function 解析VL标头(VL数据, request) {
+		if (VL数据.byteLength < 24) return;
 		if (!check_uuid(_UUID, VL数据.slice(1, 17))) { throw new Error('密钥验证失败'); }
+		await 查询DNS(request); // 只能在认证通过后才启动DNS解析，否则被DDoS攻击会拖垮DNS
+
 		const 获取数据定位 = new Uint8Array(VL数据)[17];
 		const 提取端口索引 = 18 + 获取数据定位 + 1;
 		const 建立端口缓存 = VL数据.slice(提取端口索引, 提取端口索引 + 2);
@@ -73,7 +89,7 @@ async function 启动传输管道(WS接口) {
 		for (const [目标地址, 目标端口] of 目标集) {
 			try {
 				TCP接口 = connect({ hostname: 目标地址, port: 目标端口 });
-				await Promise.race([TCP接口.opened, new Promise((_, reject) => setTimeout(() => reject(new Error(`连接超时`)), 2000))]);
+				await Promise.race([TCP接口.opened, new Promise((_, reject) => setTimeout(() => reject(new Error(`连接超时`)), 1500))]);
 				const 项 = 反代MAP.get(目标地址);
 				if (项?.失败次数 > 0) 项.失败次数 = 0;
 				break;
@@ -96,9 +112,9 @@ async function 启动传输管道(WS接口) {
 		);
 	}
 }
-let 链接SEARCH = '', 正在刷新 = false, 缓存时间 = new Date(1986, 9, 1), 反代MAP = new Map();
+let 正在刷新 = false, 缓存时间 = new Date(1986, 9, 1), 重置码 = new Set(), 反代MAP = new Map();
 import { connect } from 'cloudflare:sockets';
-const isValidDomain = (hostname) => /^(?:(?:(?:[a-zA-Z0-9][a-zA-Z0-9-_]{0,61})?[a-zA-Z0-9]\.)*(?:xn--)?[a-zA-Z0-9][a-zA-Z0-9-]{0,61}[a-zA-Z0-9]\.(?:xn--)?[a-zA-Z]{2,})$/.test(hostname);
+const isIP = (ip) => /^(\d{1,3}\.){3}\d{1,3}$/.test(ip) || ip.startsWith('[');
 async function getDnsRecord(domain, type) {
 	const apis = [
 		`https://cloudflare-dns.com/dns-query?name=${domain}&type=${type}`, `https://dns.google/resolve?name=${domain}&type=${type}`,
