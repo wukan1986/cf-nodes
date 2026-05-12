b5d7c94cbee2619e3cd6309fa610b7f7806d7380
Kan <wu-kan@163.com>
2026-05-12 16:11:22 +0800

    有internal error但还能用

--- vless_trojan_ss_worker.js
+++ vless_trojan_ss_worker.js
@@ -2,30 +2,25 @@
 
 export default {
 	async fetch(request, env) {
-		try {
-			const url = new URL(request.url); if (!路径) { UUID = env.UUID || TLS下认证可简化; 路径 = `/${encodeURIComponent(UUID)}`; }
-			if (url.pathname.startsWith(路径)) {
-				if (request.headers.get('Upgrade') === 'websocket') { url.search = url.search.replace(/{colo}/g, request.cf.colo).replace(/{country}/g, request.cf.country); return await 升级WS请求(url, request.headers.get('sec-websocket-protocol')); }
-				return new Response(创建链接1(url.hostname, UUID).replace(/pro-to-col/g, VL) + "\n\n" + 创建链接1(url.hostname, UUID).replace(/pro-to-col/g, TR), { status: 404 }); // + "\n\n" + 创建链接2(url.hostname, UUID).replace(/pro-to-col/g, SS)
-			}
-			return new Response(`Not Found. ${request.cf.country}, ${request.cf.region}, ${request.cf.colo}`, { status: 404 });
-		} catch (err) {
-			console.log(err.stack); return new Response(err.stack, { status: 500 });
+		const url = new URL(request.url); if (!路径) { UUID = env.UUID || TLS下认证可简化; 路径 = `/${encodeURIComponent(UUID)}`; }
+		if (url.pathname.startsWith(路径)) {
+			if (request.headers.get('Upgrade') === 'websocket') { url.search = url.search.replace(/{colo}/g, request.cf.colo).replace(/{country}/g, request.cf.country); return await 升级WS请求(url, request.headers.get('sec-websocket-protocol')); }
+			return new Response(订阅网页(url.hostname, UUID), { status: 404 });
 		}
+		return new Response(`Not Found. ${request.cf.country}, ${request.cf.region}, ${request.cf.colo}`, { status: 404 });
 	},
 };
 async function 升级WS请求(url, ed) {
 	const [客户端, WS接口] = Object.values(new WebSocketPair()); WS接口.accept(); WS接口.binaryType = 'arraybuffer';
-	const 协议 = url.pathname.split('/').pop(); if (协议 === VL) { WS接口.send(new Uint8Array([0, 0]).buffer) };
-	启动传输管道(WS接口, url, ed, 协议); return new Response(null, { status: 101, webSocket: 客户端 });
+	const 协议 = url.pathname.split('/').pop(); 启动传输管道(WS接口, url, ed, 协议).catch(() => { }); return new Response(null, { status: 101, webSocket: 客户端 });
 }
 async function 启动传输管道(WS接口, url, ed, 协议) {
 	let TCP接口, 传输数据, 首包数据 = true; let cancelled = false; const abort = new AbortController();
-	const close = (err, print = false) => { if (print) console.log(err); if (cancelled) return; cancelled = true; abort.abort(); try { TCP接口?.close(); } catch { } try { WS接口?.close(); } catch { } WS接口 = TCP接口 = null; };
+	const close = (err, print = false) => { if (print && err) console.log(err); if (cancelled) return; cancelled = true; abort.abort(); try { TCP接口?.close(); } catch { } try { WS接口?.close(); } catch { } WS接口 = TCP接口 = null; };
 	new ReadableStream({
 		start(controller) {
-			WS接口.addEventListener('message', (event) => { if (cancelled) return; controller.enqueue(event.data); }, { signal: abort.signal });
-			WS接口.addEventListener('close', (e) => { if (cancelled) return; controller.close(); close(e); }, { signal: abort.signal });
+			WS接口.addEventListener('message', (e) => { if (cancelled) return; try { controller.enqueue(e.data); } catch { } }, { signal: abort.signal });
+			WS接口.addEventListener('close', (e) => { if (cancelled) return; controller.close(); }, { signal: abort.signal });
 			WS接口.addEventListener('error', (e) => { if (cancelled) return; controller.error(e) }, { signal: abort.signal }); // if (ed) { controller.enqueue(Uint8Array.fromBase64(ed, { alphabet: 'base64url' }).buffer); }
 		},
 		cancel() { abort.abort(); cancelled = true; },
@@ -57,25 +52,19 @@ async function 启动传输管道(WS接口, url, ed, 协议) {
 			}
 		}
 		if (!连接成功) throw new Error(`无法连接到目标服务器: ${hostname}:${port} - 目标集长度：${目标集.length}`);
-		建立传输管道(data, is_udp, hostname);
+		if (协议 === VL) { if (WS接口.readyState === WebSocket.OPEN) WS接口.send(new Uint8Array([0, 0]).buffer) };
+		建立传输管道(data, is_udp, hostname).catch(close);
 	}
 	async function 建立传输管道(写入初始数据, is_dns, hostname) {
 		传输数据 = TCP接口.writable.getWriter();
-		if (写入初始数据.length > 0) { await 传输数据.write(写入初始数据); }
-		const reader = TCP接口.readable.getReader({ mode: 'byob' });
-		const BYOB缓冲区大小 = 1024 * 256, 系统最大4KB = 4096, BYOB安全阈值 = BYOB缓冲区大小 - 系统最大4KB;
-		let buffer = new ArrayBuffer(BYOB缓冲区大小), offset = 0, lastReadTime = performance.now(); let chunk; let timeout = TIMEOUTS.read_short;
 		try {
-			while (!cancelled) {
-				const view = new Uint8Array(buffer, offset, 系统最大4KB);
-				const { value, done } = await withTimeout(reader.read(view), timeout, `TCP read 超时 ${hostname}`); if (done) break; timeout = TIMEOUTS.read_long;
-				buffer = value.buffer; offset += value.byteLength;
-				if (value.byteLength < 系统最大4KB || performance.now() - lastReadTime >= 50 || offset > BYOB安全阈值) {
-					chunk = new Uint8Array(buffer, 0, offset); offset = 0; lastReadTime = performance.now();
-				} else { continue; }
-				if (WS接口.readyState === WebSocket.OPEN) { WS接口.send(chunk); }
-			}
-		} finally { reader.releaseLock(); 传输数据.releaseLock(); 传输数据 = null; }
+			if (写入初始数据?.byteLength > 0) { await 传输数据.write(写入初始数据); }
+			await TCP接口.readable.pipeTo(
+				new WritableStream({
+					write(chunk) { if (WS接口?.readyState === WebSocket.OPEN) WS接口.send(chunk); },
+				}),
+			);
+		} finally { try { 传输数据?.releaseLock(); } catch (e) { } 传输数据 = null; }
 	}
 }
 function addr_vl(数据) {
@@ -119,7 +108,7 @@ function addr_ss(数据) {
 async function 查询反代IP(url) {
 	const search = url.search; let cache = cacheMap.get(search); if (!cache) { cache = new IPCache(search); cacheMap.set(search, cache); }
 	const IPs = cache.IPs; if (正在刷新) return IPs; const 当前时间 = new Date(); const 分钟差 = (当前时间.getTime() - cache.Time.getTime()) / 60000;
-	if (IPs.size >= 30 || 分钟差 <= 10) return IPs; 正在刷新 = true; console.log('初始 IP总量:', IPs.size);
+	if (IPs.size >= 15 || 分钟差 <= 15) return IPs; 正在刷新 = true; console.log('初始 IP总量:', IPs.size);
 	try {
 		(await params_A_AAAA(url.searchParams, 'AAAA')).map(({ hostname, port }) => { IPs.has(hostname) || IPs.set(hostname, { 端口: port, 失败次数: 0 }) }); console.log('AAAA=IP总量:', IPs.size);
 		(await params_A_AAAA(url.searchParams, 'A')).map(({ hostname, port }) => { IPs.has(hostname) || IPs.set(hostname, { 端口: port, 失败次数: 0 }) }); console.log('A=IP总量:', IPs.size);
@@ -148,16 +137,13 @@ async function params_A_AAAA(searchParams, type) { return (await Promise.all(sea
 async function params_TXT(searchParams) { return (await Promise.all(searchParams.getAll('TXT').map(r => dns_txt(r, 'TXT')))).flat(); }
 async function params_url(searchParams) { return (await Promise.all(searchParams.getAll('url').map(r => url_txt(r)))).flat(); }
 class IPCache { constructor(search) { this.Search = search; this.Time = new Date(1986, 9, 1); this.IPs = new Map(); } }
-import { connect } from 'cloudflare:sockets'; let 正在刷新 = false, 路径 = null, UUID = null; const cacheMap = new Map(), DNS目标集 = [{ hostname: "8.8.4.4", port: 53 }, { hostname: "1.0.0.1", port: 53 }]; const TIMEOUTS = { connect: 1000, read_long: 180_000, read_short: 20_000 };
+import { connect } from 'cloudflare:sockets'; let 正在刷新 = false, 路径 = null, UUID = null; const cacheMap = new Map(), DNS目标集 = [{ hostname: "8.8.4.4", port: 53 }, { hostname: "1.0.0.1", port: 53 }]; const TIMEOUTS = { connect: 1000, read_long: 85_000, read_short: 20_000 };
 const rev = s => s.split('').reverse().join('').toLowerCase(); const AAAA = rev('344:TeN.SsSsUiLmC.PiYxOrP'), VL = rev('SsElV'), TR = rev('NaJoRt'), SS = rev('sS'), V2 = rev('nIgUlP-yAr2v');
-function sleepReject(ms, msg) { return new Promise((_, reject) => setTimeout(() => reject(new Error(msg)), ms)); } function withTimeout(promise, ms, message) { return Promise.race([promise, sleepReject(ms, message),]); }
-function 创建链接1(hostname, uuid) {
-	const v1 = new URL(`pro-to-col://12345678-1234-1234-1234-123456789012${String.fromCharCode(64)}www.wto.org:443?security=tls&sni=${hostname}&fp=chrome&type=ws&host=${hostname}#CF-VL-TR`);
-	const v2 = new URL(`url://127.0.0.1:80/${uuid}/pro-to-col`); v2.searchParams.set('AAAA', AAAA); v2.searchParams.set('A', `{colo}.${AAAA}`);
-	v1.searchParams.set('ech', "cloudflare-ech.com+https://223.5.5.5/dns-query"); v1.searchParams.set('path', v2.pathname + v2.search); return v1.href;
+function withTimeout(promise, ms, message) {
+	const controller = new AbortController(); const timer = setTimeout(() => controller.abort(new Error(message)), ms); const signal = controller.signal;
+	return Promise.race([promise, new Promise((_, reject) => { signal.addEventListener('abort', () => reject(signal.reason || new Error(message))); })]).finally(() => clearTimeout(timer));
 }
-function 创建链接2(hostname, uuid) {
-	const v1 = new URL(`pro-to-col://bm9uZToxMjM0NTY3OC0xMjM0LTEyMzQtMTIzNC0xMjM0NTY3ODkwMTI${String.fromCharCode(64)}www.wto.org:443#CF-SS`);
-	const v2 = new URL(`url://127.0.0.1:80/${uuid}/pro-to-col`); v2.searchParams.set('AAAA', AAAA); v2.searchParams.set('A', `{colo}.${AAAA}`);//ech无法导入但可配置
-	v1.searchParams.set('plugin', `${V2};mode=websocket;host=${hostname};path=${decodeURIComponent(v2.pathname + v2.search)};tls;mux=0`); return v1.href;
-}
+function ws_path(uuid, AAAA) { const v2 = new URL(`url://127.0.0.1:80/${uuid}/pro-to-col`); v2.searchParams.set('AAAA', AAAA); v2.searchParams.set('A', `{colo}.${AAAA}`); return decodeURIComponent(v2.pathname + v2.search); }
+function 基础链接1(hostname, path, is_tls, protocol) { const v1 = new URL(`pro-to-col://12345678-1234-1234-1234-123456789012${String.fromCharCode(64)}www.wto.org:${is_tls ? 443 : 80}?security=${is_tls ? 'tls' : 'none'}&sni=${hostname}&fp=chrome&type=ws&host=${hostname}#CF-pro-to-col`); v1.searchParams.set('ech', "cloudflare-ech.com+https://223.5.5.5/dns-query"); v1.searchParams.set('path', path); return v1.href.replace(/pro-to-col/g, protocol); };
+function 基础链接2(hostname, path, is_tls, protocol) { const v1 = new URL(`pro-to-col://bm9uZToxMjM0NTY3OC0xMjM0LTEyMzQtMTIzNC0xMjM0NTY3ODkwMTI${String.fromCharCode(64)}www.wto.org:${is_tls ? 443 : 80}#CF-pro-to-col`); v1.searchParams.set('plugin', `${V2};mode=websocket;host=${hostname};path=${path};${is_tls ? 'tls;' : ''}mux=0`); return v1.href.replace(/pro-to-col/g, protocol); };
+function 订阅网页(hostname, uuid) { const path = ws_path(uuid, AAAA); return `=== TLS ===\n\n${基础链接1(hostname, path, true, VL)}\n\n${基础链接1(hostname, path, true, TR)}\n\n${基础链接2(hostname, path, true, SS)}\n\n\n=== NO TLS ===\n\n${基础链接1(hostname, path, false, VL)}\n\n${基础链接2(hostname, path, false, SS)}`; }
